a98891f336e401372748ed67123f06c80e4f1bcf
serene <win.ibiz234@gmail.com>
2017-11-26 14:12:32 +0700

    ..

--- QuickstartApp/ViewController.swift
+++ QuickstartApp/ViewController.swift
@@ -5,35 +5,39 @@ import UIKit
 class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
     
     
-    // If modifying these scopes, delete your previously saved credentials by
-    // resetting the iOS simulator or uninstall the app.
+// If modifying these scopes, delete your previously saved credentials by
+// resetting the iOS simulator or uninstall the app.
     private let scopes = [kGTLRAuthScopeSheetsSpreadsheetsReadonly]
     
     private let service = GTLRSheetsService()
     let signInButton = GIDSignInButton()
-    let output = UITextView()
+//    let output = UITextView()
+
+    @IBOutlet weak var outputTextView: UITextView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+
+////   *viewDidLoad....*viewDidLoad....* *viewDidLoad....*viewDidLoad....**viewDidLoad....*viewDidLoad....**viewDidLoad....*viewDidLoad....*
+    
+override func viewDidLoad() {
+    super.viewDidLoad()
         
         // Configure Google Sign-in.
-        GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().scopes = scopes
-        GIDSignIn.sharedInstance().signInSilently()
-        
+                GIDSignIn.sharedInstance().delegate = self
+                GIDSignIn.sharedInstance().uiDelegate = self
+                GIDSignIn.sharedInstance().scopes = scopes
+                GIDSignIn.sharedInstance().signInSilently()
+    
         // Add the sign-in button.
-        view.addSubview(signInButton)
-        
-        // Add a UITextView to display output.
-        output.frame = view.bounds
-        output.isEditable = false
-        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        output.isHidden = true
-        view.addSubview(output);
+                view.addSubview(signInButton)
+    
     }
     
+// *..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*.. //
+    
+    
+////  *allFunctions.....*allFunctions....*allFunctions....*allFunctions....*allFunctions....*allFunctions....*allFunctions....*allFunctions....*allFunctions....
+    
+//SIGN-IN
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
               withError error: Error!) {
         if let error = error {
@@ -41,70 +45,71 @@ class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
             self.service.authorizer = nil
         } else {
             self.signInButton.isHidden = true
-            self.output.isHidden = false
+            self.outputTextView.isHidden = false
             self.service.authorizer = user.authentication.fetcherAuthorizer()
-            listMajors()
-        }
-    }
-    
-    // Display (in the UITextView) the names and majors of students in a sample
-    // spreadsheet:
-    // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    func listMajors() {
-        output.text = "Getting sheet data..."
-        let spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-        let range = "Class Data!A2:E"
-        let query = GTLRSheetsQuery_SpreadsheetsValuesGet
-            .query(withSpreadsheetId: spreadsheetId, range:range)
-        service.executeQuery(query,
-                             delegate: self,
-                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:))
-        )
-    }
-    
-    // Process the response and display output
-    @objc func displayResultWithTicket(ticket: GTLRServiceTicket,
-                                 finishedWithObject result : GTLRSheets_ValueRange,
-                                 error : NSError?) {
-        
-        if let error = error {
-            showAlert(title: "Error", message: error.localizedDescription)
-            return
-        }
-        
-        var majorsString = ""
-        let rows = result.values!
-        
-        if rows.isEmpty {
-            output.text = "No data found."
-            return
-        }
-        
-        majorsString += "Name, Major:\n"
-        for row in rows {
-            let name = row[0]
-            let major = row[4]
             
-            majorsString += "\(name), \(major)\n"
+          listAll()
+            
         }
-        
-        output.text = majorsString
+    }
+
+                                        //Helper Alert
+                                            func showAlert(title : String, message: String) {
+                                                let alert = UIAlertController(
+                                                    title: title,
+                                                    message: message,
+                                                    preferredStyle: UIAlertControllerStyle.alert
+                                                )
+                                                let ok = UIAlertAction(
+                                                    title: "OK",
+                                                    style: UIAlertActionStyle.default,
+                                                    handler: nil
+                                                )
+                                                alert.addAction(ok)
+                                                present(alert, animated: true, completion: nil)
+                                            }
+
+
+//Process the response and Display output
+        @objc func displayResultWithTicket(ticket: GTLRServiceTicket,
+                                           finishedWithObject result : GTLRSheets_ValueRange,
+                                           error : NSError?) {
+    
+            if let error = error {
+                showAlert(title: "Error", message: error.localizedDescription)
+                return
+            }
+    
+            outputTextView.text = result.values!.description
+            
+        }
+    
+//Get Data from Google Sheets
+    func listAll() {
+        let spreadsheetId = "1z8IlNL4SC-XeYiZRG_uSbEnbBycoHMfzensFlIsQaqs"
+        let range = "A1:U300"
+        let query = GTLRSheetsQuery_SpreadsheetsValuesGet.query(withSpreadsheetId: spreadsheetId, range:range)
+        service.executeQuery(query, delegate: self, didFinish: #selector(ViewController.displayResultWithTicket(ticket:finishedWithObject:error:))
+        )
+    }
+    
+//newVC
+    func navigateToNewVC() {
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newVC = storyBoard.instantiateViewController(withIdentifier: "newVC")
+        self.present(newVC, animated: true, completion: nil)
+    }
+    
+//Button navigate to NewVC
+    @IBAction func navigateToNewVCButton(_ sender: UIButton) {
+        navigateToNewVC()
+    }
+
+//search field
+    @IBAction func searchField(_ sender: UITextField) {
     }
     
     
-    // Helper for showing an alert
-    func showAlert(title : String, message: String) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: UIAlertControllerStyle.alert
-        )
-        let ok = UIAlertAction(
-            title: "OK",
-            style: UIAlertActionStyle.default,
-            handler: nil
-        )
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
-    }
-}
+// *..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*..*.. //
+    
+} // <-- Class Close
---
+++ QuickstartApp/reference.swift
@@ -0,0 +1,11 @@
+
+/*
+https://sheets.googleapis.com/v4/spreadsheets/1z8IlNL4SC-XeYiZRG_uSbEnbBycoHMfzensFlIsQaqs
+ 
+ sheets name: importsummary
+ sheets id: 1z8IlNL4SC-XeYiZRG_uSbEnbBycoHMfzensFlIsQaqs
+ 
+
+
+ 
+*/
